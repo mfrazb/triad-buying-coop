f2673b0d5fb53123e1cff612f98921742229df5d
Meredith Britt - Mac <118227496+mfrazb@users.noreply.github.com>
2023-08-28 00:16:32 -0400

    Connect front and back end with proxy on webpack config file

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -4,17 +4,39 @@
  * @module  User
  * @author
  * @date
- * @description component that renders particular user
+ * @description component that renders list of users
  *
  * ************************************
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const User = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = () => {
+    fetch('/api/users')
+      .then(resp => resp.json())
+      .then(data => setUsers(data))
+      .catch(error => console.log(error));
+  };
+
+  useEffect(fetchUsers, []);
+
+  const usersDivs = users.map(user => {
+    return (
+      <p>
+        Username: {user.username}, Age: {user.age}
+      </p>
+    );
+  });
+
+  console.log(usersDivs);
+
   return (
     <div>
-      <h1>User Component</h1>
+      <h1>User Components here we go!</h1>
+      {usersDivs}
     </div>
   );
 };
